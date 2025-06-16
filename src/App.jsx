ff6a3ea7e3ac7a8296d89dfaaea29faaebe44ffe
RamonArents <ramonarents@hotmail.com>
2025-06-16 22:02:06 +0200

    Initial page

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const App = () => (
+    <main>
+        <div className="pattern" />
+        <div className="wrapper">
+            <header>
+                <h1>Find <span className="text-gradient">Movies</span> You'll Enjoy Without the Hasle</h1>
+            </header>
+            <p>Search</p>
+        </div> 
+    </main>
+);
+
+export default App;
